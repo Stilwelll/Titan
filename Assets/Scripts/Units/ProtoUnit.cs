53f8b0c454491e0de35fd8d79a0862a2a52ddc68
Stilwelll <100096883+Stilwelll@users.noreply.github.com>
2023-03-22 12:02:59 -0400

    prototype-v0.0.8
    Added:
    - working targeting for friendly units
    Modified:
    - N/A
    Known Issues:
    - N/A
    ----------------------
    Next Update Focus:
    - buildings

--- Assets/Scripts/Units/ProtoUnit.cs
+++ Assets/Scripts/Units/ProtoUnit.cs
@@ -17,7 +17,6 @@ public class ProtoUnit : MonoBehaviour
     public GameObject enemy;
     public GameObject projectile;
     public GameObject gunMuzzle;
-    public GameObject unitFront;
 
     // accessing the enemy tracker table
     private UnitManager enemiesTableDict;
@@ -26,9 +25,7 @@ public class ProtoUnit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemiesTableDict = GameObject.Find("Event System").GetComponent<UnitManager>();
-        enemyTableIndex = enemiesTableDict.enemyUnitTable.Count - 1;
-
+        // if their is an enemy present invoke the shooting method
         if (enemy ?? null)
         {
             InvokeRepeating("ShootProjectile", 1, 1);
@@ -38,6 +35,7 @@ public class ProtoUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if their is an present, execute the associated method
         if (enemy ?? null)
         {
             EnemyDetection();
@@ -46,14 +44,16 @@ public class ProtoUnit : MonoBehaviour
 
     void EnemyDetection()
     {
+        // if their is more than 1 enemy, find the closest enemy and target him
+        GameObject closetEnemy;
 
-        // broken v0.0.8
         if (GameObject.FindGameObjectsWithTag("Enemy").Length > 1 & Vector3.Distance(transform.position, enemy.transform.position) <= detectionRange)
         {
-            GameObject closetEnemy = GetClosestObject();
+            closetEnemy = GetClosestObject();
 
             while (closetEnemy ?? null)
             {
+                // if the unit is within a certain distance then they will move towards the enemy until a set distance
                 if (Vector3.Distance(transform.position, closetEnemy.transform.position) <= detectionRange & Vector3.Distance(transform.position, closetEnemy.transform.position) > 2)
                 {
                     enemyTargeted = true;
@@ -62,26 +62,21 @@ public class ProtoUnit : MonoBehaviour
                 }
                 break;
             }
-
-            // if (Vector3.Distance(transform.position, closetEnemy.transform.position) <= detectionRange & Vector3.Distance(transform.position, closetEnemy.transform.position) > 2)
-            // {
-            //     enemyTargeted = true;
-            //     transform.LookAt(closetEnemy.transform);
-            //     transform.position = Vector3.MoveTowards(transform.position, closetEnemy.transform.position, unitSpeed * Time.deltaTime);
-            // }
-
-            // if (closetEnemy ?? null)
-            // {
-            //     closetEnemy = GetClosestObject();
-            // }
-
         }
-        if (GameObject.FindGameObjectsWithTag("Enemy").Length == 1 & Vector3.Distance(transform.position, enemy.transform.position) <= detectionRange & Vector3.Distance(transform.position, enemy.transform.position) > 2)
+
+        closetEnemy = GetClosestObject();
+
+        // if there is only one enemy, target that enemy
+        if (closetEnemy ?? null)
         {
-            transform.LookAt(enemy.transform);
-            transform.position = Vector3.MoveTowards(transform.position, enemy.transform.position, unitSpeed * Time.deltaTime);
+            // if the unit is within a certain distance then they will move towards the enemy until a set distance
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 1 & Vector3.Distance(transform.position, closetEnemy.transform.position) <= detectionRange & Vector3.Distance(transform.position, closetEnemy.transform.position) > 2)
+            {
+                transform.LookAt(closetEnemy.transform);
+                transform.position = Vector3.MoveTowards(transform.position, closetEnemy.transform.position, unitSpeed * Time.deltaTime);
+            }
         }
-        // if the unit is within a certain distance then they will move towards the enemy until a set distance
+        // if there is no enemy, this unit is not targeting anything, this variable is used by the projectile function
         if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
         {
             enemyTargeted = false;
@@ -90,6 +85,7 @@ public class ProtoUnit : MonoBehaviour
 
     void ShootProjectile()
     {
+        // if an enemy is a target, start shooting
         if (enemyTargeted)
         {
             Instantiate(projectile, gunMuzzle.transform.position, gunMuzzle.transform.rotation);
@@ -107,9 +103,9 @@ public class ProtoUnit : MonoBehaviour
         for (int i = 0; i < MyListOfObjects.Length; i++)  //list of gameObjects to search through
         {
             float dist = Vector3.Distance(MyListOfObjects[i].transform.position, transform.position);
-            if (dist <= closest)
+            if (dist <= closest) // if the object is closer than the detection range
             {
-                closest = dist;
+                closest = dist; 
                 closestObject = MyListOfObjects[i];
             }
         }
